17e57950f5a68decf8ba995d0f83d0433f2ca365
Greg Michnikov <greg.michnikov@gmail.com>
2016-10-14 13:16:47 -0400

    Remove react-infinite scrolling for now

--- frontend/components/suggestions/suggestion_feed.jsx
+++ frontend/components/suggestions/suggestion_feed.jsx
@@ -151,9 +151,7 @@ class SuggestionFeed extends React.Component {
         </section>
 
 				<ul className="suggestion-feed-items">
-          <Infinite elementHeight={300} useWindowAsScrollContainer >
-            {feed}
-          </Infinite>
+          {feed}
 				</ul>
 
 			</section>
